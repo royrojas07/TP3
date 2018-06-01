f91d8f6f4c38b26547012adfd7bbdfac00a78a6d
royrojas07 <38670379+royrojas07@users.noreply.github.com>
2018-05-31 22:51:47 -0700

    recuperacion

--- Vector.cpp
+++ Vector.cpp
@@ -1,116 +1,48 @@
-#include "Vector.h"
-#include <iomanip>
-#include <stdexcept>
-#include <cmath>
-using namespace std;
-
-Vector::Vector()
-: vector(nullptr)
-{
-}
-
 double Vector::distancia(Elemento *otro)
 {
 	Vector *v = dynamic_cast<Vector *>(otro);
-	/*La distancia entre dos vectores es la norma del vector diferencia.*/
-	double distancia = norma(*v-this);
+	/*La distancia entre dos vectores es la norma del vector diferencia, normalizada para conseguir 
+	un valor entre cero y uno.*/
+	double distancia = norma((*v-this).vector, (*v-this).size)/max(norma(v->vector, v->size), norma(this->vector, size));
 	return distancia;
 }
 
-Vector::Vector(const Vector &otro) 
-: vector(nullptr)
-{
-	size = otro.size;
-	vector = new double[size];
-	if(vector){
-		for(int i = 0; i < size; i++)
-			vector[i] = otro.vector[i];
-	}
-}
-
-Vector::~Vector()
-{
-	if(vector)
-		delete vector;
-}
-Elemento *Vector::clonar()
-{
-	return new Vector(*this);
-}
-
-int Vector::length() const
-{
-	return size;
-}
-
-istream &Vector::cargar(istream &entrada)
-{
-	char buffer[LIMIT];
-	entrada.getline(buffer, LIMIT);
-	size = 1 + contarEspacios(buffer);
-	if(vector)
-		delete [] vector;
-	vector = new double[size];
-	rellenar(vector, size, buffer);
-	return entrada;
-}
-
 ostream &Vector::imprimir(ostream &salida)
-{
-	salida << "[ ";
-	cout << fixed << setprecision(2);
-	for(int i = 0; i < size; i++){
-		if(i < size-1)
-			salida << vector[i] << ", ";
-		else
-			salida << vector[i];
-	}
-	salida << " ]";
 	return salida;
 }
 
 int Vector::contarEspacios(char *tira){
+int Vector::contarEspacios(const char *tira){
 	const char BLANCO = ' ';
 	int blancos = 0;
 	for(int i = 0; tira[i]; i++){
 		blancos += (tira[i] == BLANCO? 1 : 0);
+		blancos += (tira[i] == BLANCO? 1 : 0);	
 	}
 	return blancos;
 }
-
-void Vector::rellenar(double *vector, int largo, char* buffer){
-	int leidos = 0;
-	const char BLANCO = ' ';
-	string numero = "";
-	for(int i = 0; leidos < largo && i < LIMIT; i++){
-		if(buffer[i] != BLANCO && buffer[i]){
-			numero += buffer[i];
-		}else{
-			if(numero != ""){
-				vector[leidos] = stod(numero);
-				numero = "";
-			}
-			leidos++;
-		}
-	}
-}
-
-Vector &Vector::operator-(const Vector *otro)
-{	/*No se pueden restar vectores de diferente largo.*/
-	Vector *copia = dynamic_cast<Vector *>(this->clonar());
-	if(size != otro->size)
-		throw invalid_argument("Error! Los vectores difieren en cantidad de elementos!");
-	for(int i = 0; i < size; i++){
-		copia->vector[i] -= otro->vector[i];
-	}
+@ -106,11 +107,20 @@ Vector &Vector::operator-(const Vector *otro)
 	return *copia;
 }
 
 double Vector::norma(Vector &v) const
+double Vector::norma(double *v, int largo)
 {
 	double raiz = 0;
 	for(int i = 0; i < size; i++){
 		raiz += pow(v.vector[i], 2);
+	long double raiz = 0;
+	for(int i = 0; i < largo; i++){
+		raiz += pow(v[i], 2);
 	}
 	return sqrt(raiz);
+}
+
+double Vector::productoPunto(double *u, double *v, int largoU, int largoV)
+{
+	double producto = 0;
+	for(int i = 0; i < largoU && i < largoV; i++){
+		producto += u[i] * v[i]; 
+	}
+	return producto;
 }
