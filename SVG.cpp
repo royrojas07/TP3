b81a75909014d0808273276435284929751ce37f
sebas3552 <38670360+sebas3552@users.noreply.github.com>
2018-06-09 00:16:27 -0600

    Add files via upload

--- SVG.cpp
+++ SVG.cpp
@@ -1,52 +1,60 @@
 #include "SVG.H"
-#include <string>
 using namespace std;
 
+SVG::SVG()
+: grupos_length(0)
+{
+	colores[0] = "red";
+	colores[1] = "green";
+	colores[2] = "blue";
+	colores[3] = "silver";
+	colores[4] = "lime";
+	colores[5] = "maroon";
+	colores[6] = "orange";
+	colores[7] = "aqua";
+	colores[8] = "purple";
+}
+
+SVG::~SVG()
+{
+	if(s)
+		delete [] s;
+}
 void SVG::visualizar(Lista *grupos)
 {
 	if(!grupos)
 		return;
-	fstream archivo("visualizacion.html", ios::out);
-	string header = "<!DOCTYPE html>\n<html>\n<body>\n<h3>Visualización en SVG</h3>\n\n<svg width=\"2000\" height=\"2000\">";
-	string footer = "</svg>\n</body>\n</html>";
-	string textStyle = "<style>\n\ttext{\n\tfont-size: 1rem;\nfont-weight: bold\n}\nalign:center\n</style>\n";
-	/*string tabla = ""; //puede servir para algo
-	array<string, 3> colores;
-	colores[0] = "red";
-	colores[1] = "green";
-	colores[2] = "blue";
-	int cxx, cxy, cyx, cyy, czx, czy= 0;
-	int r = 100;
-	cxx = 1366/2;
-	cxy = 768/2;
-	cyx = cxx + 2*r;
-	cyy = cxy;
-	czx = (cxx + cyx)/2;
-	czy = (cyy + 2*r) - (double)(r/4);
-	int **coordenadas = new int*[3];
-	for(int j = 0; j < 3; j++)
-		coordenadas[j] = new int[2]();
-	coordenadas[0][0] = cxx;
-	coordenadas[0][1] = cxy;
-	coordenadas[1][0] = cyx;
-	coordenadas[1][1] = cyy;
-	coordenadas[2][0] = czx;
-	coordenadas[2][1] = czy;
-	archivo << header << textStyle << endl;
-	for(int i = 0; i < 3; i++){
-		archivo << "<circle cx=\"" << coordenadas[i][0] << "\" cy=\"" << coordenadas[i][1] <<"\" r=\"" << r << "\" stroke=\"black\" fill=\""<< colores[i] << "\"/>\n";
+	grupos_length = KMeans::lista_length(grupos);
+	int height = 100 * grupos_length;
+	fstream archivo("visualizacion.svg", ios::out);
+	archivo << "<?xml version='1.0' encoding='UTF-8' ?>\n<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='1366px' height='" << height <<"px'>\n";
+	int x = 1366/2; 
+	int y = 100;
+	archivo << "<text x=\"" << x << "\" y=\"" << y/2 <<"\" text-anchor=\"middle\" style=\"font-family: arial; font-size: 30px; font-weight: bold;\">Visualización en SVG</text>\n" << endl;
+	srand(static_cast<unsigned int>(time(0)));
+	s = new string[grupos_length];
+	int contador = 0;
+	string colorAleatorio;
+	for(Lista::Iterator i = grupos->begin(); i != grupos->end(); ++i, contador++){
+		colorAleatorio = colores[rand() % 9];
+		s[contador] = colorAleatorio;
+		if(i != grupos->begin()){	
+			while(seRepite(colorAleatorio)){ 
+				colorAleatorio = colores[rand() % 9];
+			}
+		}	
+		archivo << "<text x=\"" << x << "\" y=\"" << y << "\" text-anchor=\"middle\" style=\"font-weight: bold; font-family: courier; font-size: 20px; fill: " << colorAleatorio << ";\">" << *i << "</text>" << endl;
+		y += 50;
 	}
-	int largoL1, largoL2, largoL3 = 0;
-	archivo << "<text x=\""<< cxx << "\" y=\"" << cyy << "\"" << " length=\"20\">1</text>" << endl;
-	archivo << footer;
+	archivo << "</svg>";
 	archivo.close();
-	for(int i = 0; i < 3; i++)
-		delete [] coordenadas[i];
-	delete [] coordenadas;
-	delete grupos;
-	*/
-	archivo << header << endl;
-	archivo << "<text x=\"100\" y=\"100\" style=\"text-align: center; fill: #ff0000; font-weight: bold;\">" << grupos << "</text>" << endl;
-	archivo << footer;
-	archivo.close();
+}
+
+bool SVG::seRepite(string color)
+{
+	for(int i = 0; i < grupos_length; i++){
+		if(s[i] == color)
+			return true;
+	}
+	return false;
 }
--- SVG.h
+++ SVG.h
@@ -4,14 +4,39 @@
 #include "Lista.h"
 #include <iostream>
 #include <fstream>
-
+#include <array>
+#include <ctime>
+#include <string>
+#include <cstdlib>
+#include "KMeans.h"
+/**@class SVG 
+* En esta clase se implementa una función para visualizar la conformación de los elementos agrupados
+* a través de una imagen en formato SVG. Se estableció que cada renglón de elementos conforma un grupo,
+* y cada uno tendrá un color distinto para identificarlo más fácilmente.
+*/
 class SVG : public Visualizador
 {
+	private:
+		/**Array con nueve colores, para colorear las hileras de los grupos.*/
+		array<string, 9> colores;
+		/**Array que guarda cuáles colores ya se han escogido aleatoriamente, para no repetirlos.*/
+		string *s;
+		/**Largo de la lista que ingresa para ser visualizada (cantidad de grupos).*/
+		int grupos_length;
+		/**Función utilitaria para evitar repetir colores en la visualización.
+		* @param color Color a comprobar si ya ha salido.
+		* @return true si el color se repite, false en caso contrario.
+		*/
+		bool seRepite(string color);
 	public:
-	/**Función que imprime cada elemento de la lista en un archivo de formato .svg, para 
-	* poder ser visualizados a través de un navegador.
-	* @param lista Lista a visualizar.
-	*/
+		/**Constructor por omisión*/
+		SVG();
+		/**Destructor*/
+		~SVG();	
+		/**Función que imprime cada elemento de la lista en un archivo de formato .svg, para 
+		* poder ser visualizados a través de un navegador.
+		* @param lista Lista a visualizar.
+		*/
 		void visualizar(Lista *grupos);
 };
 
