36237d41a20a243a6f2b1c013c97cd2548690281
sebas3552 <38670360+sebas3552@users.noreply.github.com>
2018-06-10 03:33:59 -0600

    Add files via upload

--- APP.cpp
+++ APP.cpp
@@ -5,12 +5,9 @@ APP::APP()
 {
 }
 
-APP::~APP()
-{	
-}
-
 Lista **APP::generarDuplas(Lista *original)
-{
+{	
+	/*Clona la lista para poder manipularla sin modificar la original.*/
 	Lista *lista = original->clonar();
 	cantDuplas = lista_length/2;
 	Lista **duplas = new Lista*[cantDuplas];
@@ -20,29 +17,30 @@ Lista **APP::generarDuplas(Lista *original)
 	Elemento *elementoCercano = nullptr;
 	Lista::Iterator j = lista->begin();
 	Lista::Iterator k = lista->begin();
-	Lista::Iterator r;
+	Lista::Iterator r; //Iterador de respaldo para borrar elementos.
 	int borrados = 0;
 	for(int f = 0; f < cantDuplas && j != lista->end(); ){	
 		for(int i = 0; i < lista_length-borrados; i++){	
 			k = lista->begin();
-			++k;
+			++k; //inicia comparando cada elemento con su siguiente
 			for(k; k != lista->end(); ++k){
+				/*Obtiene el elemento más cercano a j.*/
 				if((*j)->distancia(*k) <= distanciaMenor){		
 					distanciaMenor = (*j)->distancia(*k);
 					r = k;
 					elementoCercano = *k;
 				}
 			}
-			
+			/*Hace una pareja de elementos cercanos en el vector de duplas.*/
 			if(elementoCercano && elementoCercano != *j){
 				*duplas[f] += *j;				
-				*duplas[f] += elementoCercano;
-				
+				*duplas[f] += elementoCercano;			
 				lista->borrar(r);
 				lista->borrar(j);
 				borrados++;
 				f++;
 			}
+			/*Si el # de elementos es impar, agrega el último a la última pareja (el mal tercio)*/
 			if(lista_length % 2 != 0 && i == lista_length - 1 - borrados){
 				*duplas[f-1] += *j;				
 			}
@@ -57,6 +55,7 @@ Lista **APP::generarDuplas(Lista *original)
 
 Lista **APP::agruparDuplas(Lista **duplas)
 {
+	/*Vector de listas con 5 clasificaciones posibles.*/
 	Lista **grupitos = new Lista*[CLASIFICACIONES];
 	for(int i = 0; i < CLASIFICACIONES; i++)
 		grupitos[i] = new Lista();
@@ -78,6 +77,8 @@ Lista **APP::agruparDuplas(Lista **duplas)
 			else
 				continue;
 			distancia = j->distancia(k);
+			/*Obtiene la lista más cercana a la que se está evaluando y toma la dirección de su 
+			puntero para poder eliminarlo y que no se repita en la solución.*/
 			if(distancia <= distanciaMenor){
 				listaCercana = &*k;
 				distanciaMenor = distancia;
@@ -87,7 +88,9 @@ Lista **APP::agruparDuplas(Lista **duplas)
 			pareja = new Lista();
 			*pareja += j;
 			*pareja += k;
+			/*Clasifica en el lugar correspondiente dividiendo el cálculo de distancia entre 0.2.*/
 			*grupitos[(int)(distancia/0.2-1)] += pareja;
+			/*Los borra para no escogerlos de nuevo.*/
 			delete pareja;			
 			delete j;
 			delete k;
@@ -95,6 +98,7 @@ Lista **APP::agruparDuplas(Lista **duplas)
 		}
 		distanciaMenor = 1.0;
 		listaCercana = nullptr;
+		/*Caso para cuando el número de parejas es impar.*/
 		if(i == cantDuplas - 1 - borrados && cantDuplas % 2 != 0){
 			for(int t = 0; t < CLASIFICACIONES; t++){
 				distancia = duplas[i]->distancia(grupitos[t]);
--- APP.h
+++ APP.h
@@ -3,18 +3,42 @@
 #include "Lista.h"
 #include "KMeans.h"
 #include "Agrupador.h"
-
+/**@class APP
+* Clase mediante la cual se implementa un agrupador propio del grupo de trabajo.
+* El Agrupador Por Parejas (APP) inicia creando parejas de elementos estrechamente 
+* cercanos, y un trío en caso de que el número de elementos sea impar. Los añade a un 
+* vector de listas y luego pasa a la fase donde se agrupan parejas de estas listas,
+* usando como criterio de agrupamiento la distancia entre listas. Finalmente, quedan 
+* parejas de listas cercanas con parejas de elementos cercanos. Las parejas de listas 
+* cercanas pueden clasificarse en 5 rangos: distancia 0.0-0.2, 0.2-0.4, 0.4-0.6, 0.6-0.8, 0.8-1.0.
+* De esta manera se insertan estas parejas de listas en el segmento que corresponda según 
+* la distancia entre ellas.
+*/
 class APP : public Agrupador
 {
 	private:
-		Lista **generarDuplas(Lista *);
-		Lista **agruparDuplas(Lista **);
+		/**Función para generar las duplas (pares) de elementos a partir de la lista 
+		* original a agrupar.
+		* @param original Lista original a organizar en parejas.
+		* @return vector de Listas donde cada subíndice contiene una pareja de elementos.
+		*/
+		Lista **generarDuplas(Lista *original);
+		/**Función para agrupar las duplas calculando la distancia entre cada grupo.
+		* @param duplas Vector de listas (parejas) de elementos.
+		* @return Vector de Listas que contiene las parejas de parejas de elementos clasificadas en 
+		* su respectivo ámbito.
+		*/
+		Lista **agruparDuplas(Lista **duplas);
+		/**Largo de la lista.*/
 		int lista_length;
+		/**Cantidad de duplas igual a la mitad de la cantidad de elementos.*/
 		int cantDuplas;
+		/**Clasificaciones en las que puede caer una pareja de parejas de elementos.*/
 		const int CLASIFICACIONES = 5;
 	public:
+		/**Constructor por omisión.*/
 		APP();
-		~APP();
+		/**Función que agrupa los elementos utilizando las funciones utilitarias privadas.*/
 		virtual Lista *agrupar(Lista *);
 		
 };
